21504ac6e3375f0cf194bf2e94e6bf08726d003a
Jenna Sturgeon <jenna.sturgeon@yahoo.com>
2017-01-21 23:06:37 -0800

    no more alert

--- kewmorse.js
+++ kewmorse.js
@@ -1,17 +1,24 @@
-var rand, fromMorse, toEn;
+var rand, morseCode, toEn;
 var correct; //need to include addElemById;
 var next; //need to include addElemById;
 
 function getAnswer() {
+
+
 	var answer = document.getElementById('answer'); //melanie needs to create this html element
+	var uAnswer = ' '; //this might be an element who fuckin knows at this point
+
 	return answer
 }
 
-
+var wordArray;
 
 
 function generateQuestion() {
-
+	answer = getAnswer();
+	answer.value = '';
+	
+	wordArray = Math.floor(Math.random() * 1000); //this probably wont work  
 	translation();
 
 }
@@ -19,11 +26,16 @@ function generateQuestion() {
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-        alert(xhr.responseText);
-        generateQuestion();
+    	wordArray = xhr.responseText;
+    	generateQuestion();
+
     }
 }
 xhr.open('GET', 'https://mjohnson196.github.io/words.json', true);
 xhr.send(null);
+
+function checkAnswer() {
+
+}
 
 
