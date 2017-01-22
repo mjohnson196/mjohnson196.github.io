2de3e5c0a5f5dc7523bea0ed97f75a5e069ccd24
Jenna Sturgeon <jenna.sturgeon@yahoo.com>
2017-01-21 20:30:24 -0800

    added words list

--- kewmorse.js
+++ kewmorse.js
@@ -1,4 +1,28 @@
-var rand;
+var rand, fromMorse, toEn;
 var correct; //need to include addElemById;
 var next; //need to include addElemById;
-var test;
+
+function getAnswer() {
+	var answer = document.getElementById('answer'); //melanie needs to create this html element
+	return answer
+}
+
+
+
+
+function generateQuestion() {
+
+	translation();
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+	    if (xhr.readyState == XMLHttpRequest.DONE) {
+	        alert(xhr.responseText);
+	    }
+	}
+	xhr.open('GET', 'https://mjohnson196.github.io/words.json', true);
+	xhr.send(null);
+
+}
+
+
