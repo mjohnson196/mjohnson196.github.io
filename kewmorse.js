a1ede3df86d8747d20e475ae90a2d773536cd8d2
Jenna Sturgeon <jenna.sturgeon@yahoo.com>
2017-01-22 10:19:19 -0800

    test

--- kewmorse.js
+++ kewmorse.js
@@ -26,37 +26,12 @@ function generateQuestion() {
 
 }
 
-function translation(result, lValues) {
-
-var lValues = [{key:"a", value:".-"},{key:"b", value:"-..."}, {key: "c", value: "-.-."}, {key: "d", value: "-.."}, {key: "e", value: "."}, {key: "f", value: "..-."},
-{key:"g", value: "--.-"}, {key: "h", value: "...."}, {key: "i", value: ".."}, {key: "j", value: ".---"}, {key:"k", value: "-.-"}, {key:"l", value: ".-.."},
-{key: "m", value: "--"}, {key:"n", value: "-."}, {key: "o", value: "---"}, {key: "p", value: ".--."}, {key: "q", value: "--.-"}, {key: "r", value: ".-."},
-{key: "s", value: "..."}, {key: "t", value: "-"}, {key: "u", value: "..-"}, {key: "v", value: "...-"}, {key: "w", value: ".--"}, {key: "x", value: "-..-"},
-{key: "y", value: "-.--"}, {key: "z", value: "--.."}];
-
-lLength = lValues.length;
-
-	for(var i = 0; i < lValues.length; i++)
-	{
-    	result[lValues[i].key] = lValues[i].value;
-	}
-
-return result;
-
-}
-
 
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
     	wordArray = JSON.parse(xhr.responseText);
-    	translation(result, lValues, lLength);
-
-    	for (int i = 0; i < lLength; i++)
-    	{
-    		wordArray[i] = result[lValues[i]];
-    	}
-
+    	alert(translation(result, lValues));
 
     	generateQuestion();
 
